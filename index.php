54436e0e236d0b70be15ede9ba05ce54001a4776
adam-mawji <49711956+adam-mawji@users.noreply.github.com>
2019-04-17 01:06:05 -0700

    uploadded

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Adam</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:800|Open+Sans|Source+Sans+Pro:900" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="boom">
+      <img id="bigbank" src="background.jpg" alt="">
+      <img id="littlebank" src="adam.png" alt="">
+      <h1 id="text">Adam Mawji</h1>
+      <div class="borders">
+        <div class="top"> </div>
+        <div class="bottom"> </div>
+        <div class="left"> </div>
+        <div class="right"> </div>
+      </div>
+      <div class="infobox">
+        <h2>First-year University Student</h2>
+        <h2>Leadership &middot; Passion &middot; Resolve</h2>
+      </div>
+    </div>
+      <!--
+    <section id="top">
+      <h1>Adam Mawji</h1>
+      <h2>First-year University Student</h2>
+      <h2>Leadership &middot; Passion &middot; Resolve</h2>
+    </section>-->
+
+    <section id="bio">
+      <h3>About</h3>
+      <p>
+        Greetings, my name is <span class="hl">Adam Mawji</span> and I am a <span class="hl">High-School graduate</span>
+        entering the first year of university with a <span class="hl">major in Political
+        Science.</span>
+      </p>
+      <p> Throughout my life, I have
+        demonstrated <span class="hl">strong leadership
+        capabilities</span> balanced with
+        my pursuit of <span class="hl">artistic and
+        academic
+        passions.</span>
+      </p>
+      <p> As a <span class="hl">leader, participant, and engaged
+         student</span> in all
+        aspects of high school life,
+        <span class="hl">I face new challenges with notable
+        resolve</span> and strive to attain
+        <span class="hl">the best results</span> I possibly can
+        through the myriad of ventures
+        I partake.
+      </p>
+    </section>
+
+    <section id="container">
+      <div class="leadership">
+        <div class="thumbnail lead">
+          <h4>Leadership</h4>
+        </div>
+        <div class="folddown">
+          <p>
+            Regarding leadership, the most important trait of the leader is not their own competence to complete tasks as an individual. Rather it is their ability to inspire, galvanize, and mobilize others into taking responsibilities on themselves while working as a cohesive unit to accomplish various tasks. Through these various experiences, I have developed this skill by communicating objectives clearly, being transparent in my intentions with the people I work with, and trusting my team to strive for the highest quality in their work.
+          </p>
+          <h4>Model United Nations</h4>
+          <h3>Director at PacificMUN 2017, VancouverMUN 2018, Canadian Highschools MUN 2018</h3>
+          <p>
+            Through Directing at various Model United Nations, I have been given the honour to lead hundreds of fellow students in discussing and debating the dynamic issues which plague the international society,
+          </p>
+
+          <h3>Under-Secretary-General of Committee at PacificMUN 2018</h3>
+          <p>
+            In this capacity, I was able to design the entire debate programme for the 600 attendees of the 3-day PacificMUN conference. This was an undeniable honour as it gave me the opportunity to present forth problems with such diverse global perspectives that, even though I was in a leadership role, I was still learning.
+          </p>
+
+          <h3>Chief of Staff at PacificMUN 2019</h3>
+          <p>
+            As the Chief of Staff at PacificMUN 2019, I was tasked with building the staff team of 50 high school students who would directly lead the 600 conference attendees in debate and ensure that all students arriving would leave with an enriched idea on the complex global climate we are attempting to navigate.
+          </p>
+
+          <h4>Student Council</h4>
+          <h3>Student Council Grade Representative for 5 Consecutive Terms</h3>
+          <p>
+            As a consistently re-elected Student Council Grade Representative, I had to ensure that I was not only voicing the needs of my grade to the larger student body and the Southpointe Academy Administration but actually fight and provide proof of change to be re-elected for such an extended period.
+          </p>
+
+          <h3>Southpointe Academy Head Prefect 2018/2019 School year</h3>
+          <p>
+            Entering Grade 12, I was named Head Prefect of Southpointe Academy, a role I do not take lightly. While leading the student council in event planning and organization of major events such as the Terry Fox Run and Sports Day, I am also tasked with representing the Student Body as their voice to the administration. I am genuinely humbled to be given such responsibility and continually ensure I leave the community at the end of the year better than when I found it.
+          </p>
+        </div>
+      </div>
+      <div class="passion">
+        <div class="thumbnail pass">
+          <h4>Passion</h4>
+        </div>
+        <div class="folddown">
+          <p>
+            While balancing leadership and academic pursuits, I find it pivotal to lead a balanced life. This means that the pursuit of my personal artistic passions along with a continued value for service are two of my most defining characteristics. Through various artistic pursuits and volunteering roles, I have refined my abilities in public speaking and communication while building my skills as a team player.
+          </p>
+          <h4>Artistic Passions</h4>
+          <h3>Southpointe Theatre Group</h3>
+          <p>
+            As part of the Southpointe Theatre Group, I have had the honour to go from a supporting role in major school productions to starring in the 2018 production of “The Marriage of Bette and Boo” and even learning to adapt and direct my own One-Act Play, “What We Talk About When We Talk About Love” based on the short story by Raymond Carver.
+          </p>
+
+          <h3>Southpointe Improv Team</h3>
+          <p>
+            One of the biggest experiences I have had working in a team, Improv gave me the skills necessary to learn how a group can develop synergy and have each member find their distinct niche in contributing the group's success. This experience reached its pinnacle in 2019 as the Southpointe team broke into the top 10 teams in the province, a new accomplishment in school History.
+          </p>
+
+          <h4>Volunteer Experience</h4>
+          <h3>Material Transport Team for the World Partnership Walk</h3>
+          <p>
+            For the past decade, I have grown to become more involved in the Material Transport Team for the World Partnership Walk, an annual walkathon in Vancouver raising millions of dollars to stop Global Poverty. In this role, I spend time throughout the months surrounding the event organizing and executing logistics plan to ensure the event runs smoothly for the several thousands of people who attend annually.
+          </p>
+
+          <h3>Peer Tutor</h3>
+          <p>
+            As a peer tutor, I chose to use my knowledge and success in school to help those who may be struggling, not for financial reasons, are unable to receive the help they so rightly deserve. While the extent of my impact may be more limited in this role, only interacting with a handful of people, I genuinely find that this experience is one that is transformative for both myself and the students I tutor.
+          </p>
+        </div>
+      </div>
+      <div class="resolve">
+        <div class="thumbnail res">
+          <h4>Resolve</h4>
+        </div>
+        <div class="folddown">
+          <p>
+            Throughout all the challenges I have faced, I bring one consistent thing to each, my resolve to attain the best results I possibly can. While everyone has strengths and weaknesses, I continually strive to do better than I could have yesterday, and it is this mentality which I believe sets my resolve apart from others.
+          </p>
+
+          <h4>Past Work Experience</h4>
+          <h3>Brand Ambassador for Mosaic Marketing North America</h3>
+          <p>
+            As a BA for Mosaic, I have been employed for the past two years learning about both practical and digital marketing by being actively involved in bringing activations to life for various clients spanning from Coca-Cola to Samsung.
+          </p>
+
+          <h4>Achievements and Accolades</h4>
+          <h3>Honour Roll with Distinction 5 Academic Years</h3>
+          <p>
+            Being awarded this honour for academic achievement shows how despite numerous other commitments, I still am able to balance my life to ensure that I make the most of the education I am so fortunate to receive.
+          </p>
+
+          <h3>BC Highschool Scientific Innovation Award 2016 and BC Highschool Human Genome Science Award 2017</h3>
+          <p>
+            These two awards in the field of sciences demonstrate my resolve to be a well-rounded student in all aspects.
+          </p>
+
+          <h3>Best Delegate at ConnectMUN 2015, CAHSMUN 2016, CAHSMUN 2017, CAIMUN 2017, ConnectMUN 2017.</h3>
+          <p>
+            Through these accolades at Model United Nations show how, outside of leadership, I am able to engage in thorough research and diplomacy to reach compromises in a dynamic where a plethora of perspectives are clashing.
+          </p>
+
+          <h3>AP Capstone Diploma Recipient</h3>
+          <p>
+            As a prospective recipient of the AP Capstone Diploma, I have been able to demonstrate an outstanding ability to conduct independent research that actually acknowledges a gap in the current scholarly knowledge collective
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section id="bottom">
+      <div class="bottom-container">
+        <h3>Keep in touch</h3>
+        <p>
+          To get in touch you can reach me via: <br>
+          Email: <a href="mailto:adammawji19@gmail.com">adammawji19@gmail.com</a> <br>
+          Phone: 604-808-3445 <br>
+          Linkedin: <a href="https://ca.linkedin.com/in/adam-mawji">adam-mawji</a><br><br>
+
+          References and CV availailable upon request
+        </p>
+      </div>
+    </section>
+    <script>
+      var acc = document.getElementsByClassName("thumbnail");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+        });
+      }
+    </script>
+    <script>
+      $(window).scroll(function(){
+        $("#text").css({
+          "transform": "translate(-50%, " + $(window).scrollTop() / 5 + "px)"
+        });
+        $(".infobox").css({
+          "transform": "translate(150%, " + $(window).scrollTop() / 5 + "px)"
+        });
+      });
+    </script>
+  </body>
+</html>
